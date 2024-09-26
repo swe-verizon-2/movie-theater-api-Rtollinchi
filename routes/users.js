0153dcd49ec8baeab075aa8a3a0962a5720c7372
Rtollinchi <rtollinchi23@gmail.com>
2024-09-26 19:15:59 -0400

    add User routes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const { User } = require("../models");
+const { Show } = require("../models");
 const { check, validationResult } = require("express-validator");
 
 router.get("/", async (req, res, next) => {
@@ -8,6 +9,42 @@ router.get("/", async (req, res, next) => {
     const allUser = await User.findAll();
 
     res.json(allUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id/shows", async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    const shows = await user.getShows();
+
+    res.json(shows);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:userId/show/:showId", async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId);
+    const show = await Show.findByPk(req.params.showId);
+
+    await user.addShow(show);
+
+    res.json({
+      message: `User ${user.id} is now associated with Show ${show.id}`,
+    });
   } catch (error) {
     next(error);
   }
